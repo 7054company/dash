f44e511f08a5f5df1205813201db10ca049ec809
7054company <34761088+7054company@users.noreply.github.com>
2023-11-08 11:02:20 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const dashboard = require('./dashboard'); // Import the dashboard module
+
+// Middleware for parsing form data
+app.use(express.urlencoded({ extended: false }));
+
+// Serve static files (CSS, images, etc.)
+app.use('/styles', express.static(path.join('styles'));
+
+// Set the path to your data.txt file
+const dataFilePath = path.join(__dirname, 'data.txt');
+
+// Function to read user data from data.txt
+function readUserData() {
+    const data = fs.readFileSync(dataFilePath, 'utf8');
+    const lines = data.split('\n');
+    const users = lines.map((line) => {
+        const [uid, username, password, lastLogin, lastIPs] = line.split(':');
+        return { uid, username, password, lastLogin, lastIPs: lastIPs.split(',') };
+    });
+    return users;
+}
+
+// Routes for the login page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Authenticate the user
+    const users = readUserData();
+    const user = users.find((u) => u.username === username && u.password === password);
+
+    if (user) {
+        // Set the user in the request object
+        req.user = user;
+
+        // Successful login, update the last login timestamp, IP, and redirect to the dashboard
+        const now = new Date();
+        user.lastLogin = now.toISOString();
+        const userIP = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+        user.lastIPs.push(userIP);
+        
+        // Store the updated data in data.txt (update your data.txt accordingly)
+        const updatedData = users.map((u) => `${u.uid}:${u.username}:${u.password}:${u.lastLogin}:${u.lastIPs.join(',')}`).join('\n');
+        fs.writeFileSync(dataFilePath, updatedData, 'utf8');
+
+        res.redirect('/dashboard');
+    } else {
+        // Invalid credentials, show an error message or stay on the login page
+        res.send('Invalid username or password. Please try again.');
+    }
+});
+
+// Include the dashboard routes
+app.use('/dashboard', dashboard);
+
+// ... Other routes and middleware
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
