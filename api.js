f2d083c600c8b596e6ce2ff8c329c78f3f902d65
7054company <34761088+7054company@users.noreply.github.com>
2023-11-09 21:09:21 +0530

    Update api.js

--- api.js
+++ api.js
@@ -13,7 +13,11 @@ function generateSessionId() {
   return crypto.randomBytes(16).toString('hex');
 }
 
-apiRouter.get('/login/:username/:password', async (req, res) => {
+function generateUid() {
+  return crypto.randomBytes(8).toString('hex');
+}
+
+apiRouter.post('/signup/:username/:password', async (req, res) => {
   try {
     const { username, password } = req.params;
 
@@ -24,24 +28,34 @@ apiRouter.get('/login/:username/:password', async (req, res) => {
       return { uid, username: u, password: p, sessionId };
     });
 
-    // Check if the provided username and password match any user
-    const user = users.find(u => u.username === username && u.password === password);
-
-    if (user) {
-      // Generate a unique session ID
-      const sessionId = generateSessionId();
-
-      // Store the session ID in the session cache
-      sessionCache.set(sessionId, user);
-
-      // Save the session ID in the data.txt file
-      data += `\n${user.uid} ${user.username} ${user.password} ${sessionId}`;
-      await fs.writeFile(dataFilePath, data);
-
-      res.json({ success: true, message: 'Login successful', user, sessionId });
-    } else {
-      res.status(401).json({ success: false, message: 'Invalid credentials' });
+    // Check if the username is already taken
+    if (users.some(u => u.username === username)) {
+      res.status(400).json({ success: false, message: 'Username already exists' });
+      return;
     }
+
+    // Generate a unique user ID
+    const uid = generateUid();
+
+    // Create a new user
+    const newUser = {
+      uid,
+      username,
+      password,
+      sessionId: generateSessionId(),
+    };
+
+    // Add the new user to the users array
+    users.push(newUser);
+
+    // Save the updated user data in the data.txt file
+    data += `\n${newUser.uid} ${newUser.username} ${newUser.password} ${newUser.sessionId}`;
+    await fs.writeFile(dataFilePath, data);
+
+    // Store the session ID in the session cache
+    sessionCache.set(newUser.sessionId, newUser);
+
+    res.json({ success: true, message: 'Signup successful', user: newUser });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: 'Internal server error' });
