29c3dc38841779568029f547ce36f60ec69a57f6
7054company <34761088+7054company@users.noreply.github.com>
2023-11-09 11:50:20 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,79 @@
+
+// login.js
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+function generateSessionID() {
+  return crypto.randomBytes(16).toString('hex');
+}
+
+function authenticateUser(username, password) {
+  const data = fs.readFileSync('data.txt', 'utf-8').split('\n');
+
+  for (let i = 0; i < data.length; i++) {
+    const [uid, storedUsername, storedPassword] = data[i].split(' ');
+
+    if (username === storedUsername && password === storedPassword) {
+      // Generate a new session ID for the user
+      const newSessionID = generateSessionID();
+      data[i] = `${uid} ${storedUsername} ${storedPassword} ${newSessionID}`;
+
+      // Write the updated data back to the file
+      fs.writeFileSync('data.txt', data.join('\n'));
+
+      return newSessionID;
+    }
+  }
+
+  return null;
+}
+
+module.exports = {
+  authenticateUser,
+};
+```
+
+2. In your `index.js`, you can modify the login logic accordingly:
+
+```javascript
+// index.js
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const session = require('express-session');
+const login = require('./login');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: false }));
+
+app.use(express.static(__dirname + '/views'));
+app.use('/styles', express.static(__dirname + '/styles'));
+
+app.get('/', (req, res) => {
+  // Check if the user is already logged in
+  if (req.session.uid) {
+    res.send(`Welcome, User with Session ID ${req.session.uid}`);
+  } else {
+    res.sendFile(__dirname + '/views/index.html');
+  }
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const sessionID = login.authenticateUser(username, password);
+
+  if (sessionID !== null) {
+    // Store the user's session ID in the session to keep them logged in
+    req.session.uid = sessionID;
+    res.send(`Login successful. Welcome, User with Session ID ${sessionID}`);
+  } else {
+    res.send('Login failed. Incorrect username or password.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+```
