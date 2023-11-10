1a0d18f70d5bc10d40b007d7b29c314ab06fc6a6
7054company <34761088+7054company@users.noreply.github.com>
2023-11-10 16:26:53 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,12 @@
-
 // login.js
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const fs = require('fs');
 
-// Read user data from data.txt
 const usersPath = path.join(__dirname, 'data.txt');
+
+// Read user data from data.txt
 let users = [];
 
 try {
