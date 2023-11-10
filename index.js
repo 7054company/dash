28baeeb9f849b0a0361ba6b18acb475e4fcd1e91
7054company <34761088+7054company@users.noreply.github.com>
2023-11-10 16:04:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// index.js
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+const loginRouter = require('./login');
+const dashboardRouter = require('./dashboard'); // Add this line
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 // Serve static files from the root folder
 app.use(express.static(__dirname));
@@ -11,6 +17,12 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'welcome.html'));
 });
+
+// Use the loginRouter for the /login route
+app.use('/login', loginRouter);
+
+// Use the dashboardRouter for the /dashboard route
+app.use('/dashboard', dashboardRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
