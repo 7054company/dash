ab1f84e8ee98f5289ca0c35f2ac774df97e4cc99
7054company <34761088+7054company@users.noreply.github.com>
2023-11-10 20:16:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-
 const express = require('express');
 const path = require('path');
-const dashboard = require('./dashboard'); // Import your dashboard module
 
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'view' directory
-app.use(express.static(path.join(__dirname, 'view')));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for the root page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'view', 'index.html'));
+  res.send('Get started');
 });
 
 // Route for the dashboard page
 app.get('/dashboard', (req, res) => {
-  dashboard.showDashboard(req, res);
+  res.sendFile(path.join(__dirname, 'view', 'dashboard.html'));
 });
 
-// Route for displaying the free VPS page - moved to dashboard.js
+// Route for displaying the free VPS page with iframe
 app.get('/dashboard/free-vps', (req, res) => {
-  dashboard.displayFreeVPSPage(req, res);
+  const websiteURL = 'https://6900-throbbing-dream-56293036.eu-ws3.runcode.io/vnc.html';
+  res.send(`
+    <html>
+      <head>
+        <title>Free VPS</title>
+      </head>
+      <body>
+        <h1>Welcome to Free VPS</h1>
+        <iframe src="${websiteURL}" width="100%" height="600"></iframe>
+      </body>
+    </html>
+  `);
 });
 
 // Start the server
