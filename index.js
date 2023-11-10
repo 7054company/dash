2a639f520f8d5667925d95a61e5f67ef61f328f0
7054company <34761088+7054company@users.noreply.github.com>
2023-11-10 14:53:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
-// index.js
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
-const { apiRouter, sessionCache } = require('./api');
+const path = require('path');
+const loginModule = require('./login');
+const dashboardModule = require('./dashboard');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'views')));
-app.use('/api', apiRouter);
+// Middleware to retrieve the client's IP address
+app.use((req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  req.clientIP = ip;
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-app.get('/dashboard/:sessionId', (req, res) => {
-  const { sessionId } = req.params;
-  const user = sessionCache.get(sessionId);
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/login.html'));
+});
 
-  if (user) {
-    res.send(`Welcome to the dashboard, ${user.username}!`);
-  } else {
-    res.status(401).send('Unauthorized access');
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Both username and password are required.' });
   }
+
+  const userData = loginModule.loadUserData();
+  const user = userData.find(u => u.username === username);
+
+  if (!user || user.password !== password) {
+    return res.status(401).json({ error: 'Invalid username or password.' });
+  }
+
+  // Redirect to the /dashboard route with the welcome message and IP address
+  res.redirect(`/dashboard?username=${username}&ip=${req.clientIP}`);
+});
+
+app.get('/dashboard', (req, res) => {
+  const { username, ip } = req.query;
+
+  if (!username) {
+    return res.status(400).json({ error: 'Username is required.' });
+  }
+
+  const welcomeMessage = dashboardModule.getWelcomeMessage(username, ip);
+  res.status(200).json({ message: welcomeMessage });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
