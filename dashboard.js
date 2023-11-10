aaa1ac51a4ca2a787b11b32b420c9d67fe2603af
7054company <34761088+7054company@users.noreply.github.com>
2023-11-10 16:05:46 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,24 @@
 // dashboard.js
-const getWelcomeMessage = (username, ip) => {
-  return `Welcome, ${username}! Your current IP address is ${ip}.`;
-};
+const express = require('express');
+const router = express.Router();
 
-module.exports = {
-  getWelcomeMessage,
-};
+// Dashboard route
+router.get('/', (req, res) => {
+  // You might want to retrieve user information from the session or token in a real application
+  const username = req.query.username || 'Guest';
+
+  res.status(200).send(`
+    <html>
+      <head>
+        <title>Dashboard</title>
+      </head>
+      <body>
+        <h1>Welcome to the Dashboard, ${username}!</h1>
+        <p>This is your personalized dashboard page.</p>
+        <a href="/">Back to Welcome Page</a>
+      </body>
+    </html>
+  `);
+});
+
+module.exports = router;
