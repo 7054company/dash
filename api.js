51e7d57aafdb57889dbeaee17c9d575cc6653e65
7054company <34761088+7054company@users.noreply.github.com>
2023-11-12 10:39:53 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,30 +1,15 @@
-
-// api.js
-
 const fs = require('fs');
-const jwt = require('jsonwebtoken');
-
-const secretKey = '12345'; // Replace with your actual secret key
 
 function readUserData() {
   const rawData = fs.readFileSync('data.txt');
   return JSON.parse(rawData);
 }
 
-function generateToken(user) {
-  return jwt.sign({ user }, secretKey, { expiresIn: '1h' }); // Token expires in 1 hour
-}
-
-function verifyLogin(userData, user, password) {
+function verifyLogin(user, password) {
+  const userData = readUserData();
   const foundUser = userData.find((u) => u.username === user || u.email === user);
 
   if (foundUser && foundUser.password === password) {
-    const token = generateToken({
-      uid: foundUser.uid,
-      email: foundUser.email,
-      username: foundUser.username,
-    });
-
     return {
       success: true,
       message: 'Login successful',
@@ -33,7 +18,6 @@ function verifyLogin(userData, user, password) {
         email: foundUser.email,
         username: foundUser.username,
         lastSession: foundUser.lastSession,
-        token,
       },
     };
   } else {
@@ -44,17 +28,6 @@ function verifyLogin(userData, user, password) {
   }
 }
 
-function decodeToken(token) {
-  try {
-    return jwt.verify(token, secretKey);
-  } catch (error) {
-    return null;
-  }
-}
-
 module.exports = {
-  readUserData,
-  generateToken,
   verifyLogin,
-  decodeToken,
 };
