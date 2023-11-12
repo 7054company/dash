2056cd6876b6cf06a1ab368e4b6f842fa391b7d6
7054company <34761088+7054company@users.noreply.github.com>
2023-11-12 13:12:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-
-// note4 index.js
-
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const port = 3000;
+const apiModule = require('./apiModule');
 
 app.use(express.json());
 
@@ -24,26 +21,24 @@ app.get('/dashboard', (req, res) => {
 // Route for displaying the free VPS page with iframe using /f
 app.get('/f', (req, res) => {
   const websiteURL = 'https://6900-throbbing-dream-56293036.eu-ws3.runcode.io/vnc.html';
-  const dataContent = fs.readFileSync('data.txt', 'utf-8');
-  const lines = dataContent.split('\n');
-
-  // Assume the first line in data.txt is used for Free VPS
-  const [uid, username, password, ip] = lines[0].split(' ');
-
   res.send(`
     <html>
       <head>
         <title>Free VPS</title>
       </head>
       <body>
-        <h1>Welcome to Free VPS - ${uid}</h1>
-        <p>Username: ${username}</p>
-        <p>Password: ${password}</p>
-        <p>IP: ${ip}</p>
+        <h1>Welcome to Free VPS - 7ea</h1>
         <iframe src="${websiteURL}" width="100%" height="800"></iframe>
       </body>
     </html>
   `);
+});
+
+// Route for login using the apiModule
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  const result = apiModule.loginUser(username, password);
+  res.json(result);
 });
 
 // Start the server
