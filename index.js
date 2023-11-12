c4b01ca69fe1c832ff476e031a2b22d6328a7eb2
7054company <34761088+7054company@users.noreply.github.com>
2023-11-12 10:14:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 
-// index.js
-
 const express = require('express');
 const api = require('./api'); // Assuming api.js is in the same directory
 const app = express();
 const port = 3000;
-
-app.use(express.json());
 
 // Serve static files from the 'views' directory
 app.use(express.static(__dirname + '/views'));
@@ -21,31 +17,31 @@ app.get('/dashboard', (req, res) => {
   res.sendFile(__dirname + '/views/dashboard.html');
 });
 
-// Route for the login page
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/views/login.html');
+// Route for displaying the free VPS page with iframe
+app.get('/f', (req, res) => {
+  const websiteURL = 'https://6900-throbbing-dream-56293036.eu-ws4.runcode.io/vnc.html';
+  res.send(`
+    <html>
+      <head>
+        <title>Free VPS</title>
+      </head>
+      <body>
+        <h1>Welcome to Free VPS - 7ea</h1>
+        <iframe src="${websiteURL}" width="100%" height="800"></iframe>
+      </body>
+    </html>
+  `);
 });
 
-// Route for the API login endpoint
-app.post('/api/login', (req, res) => {
-  const { user, password } = req.body;
-  const userData = api.readUserData(); // Read user data from the file
+// Route for handling login (using GET request)
+app.get('/api/login', (req, res) => {
+  const { user, password } = req.query;
 
-  const result = api.verifyLogin(userData, user, password);
-
-  res.json(result);
-});
-
-// Route for the API account endpoint
-app.get('/api/account', (req, res) => {
-  const token = req.header('Authorization');
-
-  if (!token) {
-    return res.status(401).json({ success: false, message: 'Unauthorized: Missing token' });
+  if (!user || !password) {
+    return res.json({ success: false, message: 'Username and password are required' });
   }
 
-  const result = api.getUserDetails(token);
-
+  const result = api.verifyLogin(user, password);
   res.json(result);
 });
 
