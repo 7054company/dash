df14087d9d0d01cfe6716731d2c63c9e72fe7f0f
7054company <34761088+7054company@users.noreply.github.com>
2023-11-13 16:53:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
+// index.js
 
 const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON and form data
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/views')); // Assume dashboard.html is in a "public" directory
 
-// Define a route handler for the login path
-app.post('/login', (req, res) => {
-  // Get username and password from the request body
-  const { username, password } = req.body;
-
-  // Read user data from the "data.txt" file
-  fs.readFile('data.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Internal Server Error');
-    }
-
-    // Parse the data as JSON
-    const userData = JSON.parse(data);
-
-    // Verify username and password
-    if (userData.username === username && userData.password === password) {
-      res.send('Login successful!');
-    } else {
-      res.status(401).send('Unauthorized: Invalid username or password');
-    }
-  });
+app.get('/', (req, res) => {
+  res.send('Welcome to the Application!');
 });
 
-// Start the server
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Read user and password data from data.txt
+  const data = fs.readFileSync('data.txt', 'utf8');
+  const [savedUsername, savedPassword] = data.split(',');
+
+  // Verify the credentials
+  if (username === savedUsername && password === savedPassword) {
+    res.redirect('/dashboard');
+  } else {
+    res.send('Invalid credentials. Please try again.');
+  }
+});
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(__dirname + '/views/dashboard.html');
+});
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
