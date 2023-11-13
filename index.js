5818c475ab77aec7f143d211d7ed238b31950829
7054company <34761088+7054company@users.noreply.github.com>
2023-11-13 10:30:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Define a route for /
+app.get('/', (req, res) => {
+  res.send('Welcome to the start!');
+});
 
 // Define a route for /login
 app.post('/login', (req, res) => {
@@ -24,11 +32,18 @@ app.post('/login', (req, res) => {
 
     // Check if the provided username and password match
     if (username === userData.username && password === userData.password) {
-      res.send('Login successful!');
+      // Redirect the logged-in user to /dashboard
+      res.redirect('/dashboard');
     } else {
       res.status(401).send('Invalid username or password');
     }
   });
+});
+
+// Define a route for /dashboard
+app.get('/dashboard', (req, res) => {
+  const dashboardPath = path.join(__dirname, 'views', 'dashboard.html');
+  res.sendFile(dashboardPath);
 });
 
 app.listen(port, () => {
