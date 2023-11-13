d533ba7745a3d750b38df2480ceeb68beb24ff18
7054company <34761088+7054company@users.noreply.github.com>
2023-11-13 10:17:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,36 @@
+
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
-const apiModule = require('./apiModule');
 
+// Middleware to parse incoming JSON requests
 app.use(express.json());
 
-// Serve static files from the 'views' directory
-app.use(express.static(__dirname + '/views'));
-
-// Route for the root page
-app.get('/', (req, res) => {
-  res.send('Get started');
-});
-
-// Route for the dashboard page
-app.get('/dashboard', (req, res) => {
-  res.sendFile(__dirname + '/views/dashboard.html');
-});
-
-// Route for displaying the free VPS page with iframe using /f
-app.get('/f', (req, res) => {
-  const websiteURL = 'https://6900-throbbing-dream-56293036.eu-ws3.runcode.io/vnc.html';
-  res.send(`
-    <html>
-      <head>
-        <title>Free VPS</title>
-      </head>
-      <body>
-        <h1>Welcome to Free VPS - 7ea</h1>
-        <iframe src="${websiteURL}" width="100%" height="800"></iframe>
-      </body>
-    </html>
-  `);
-});
-
-// Route for login using the apiModule
-app.post('/api/login', (req, res) => {
+// Define a route for /login
+app.post('/login', (req, res) => {
   const { username, password } = req.body;
-  const result = apiModule.loginUser(username, password);
-  res.json(result);
+
+  // Read user and password data from data.txt
+  fs.readFile('data.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    // Parse the data from data.txt (assuming JSON format for simplicity)
+    const userData = JSON.parse(data);
+
+    // Check if the provided username and password match
+    if (username === userData.username && password === userData.password) {
+      res.send('Login successful!');
+    } else {
+      res.status(401).send('Invalid username or password');
+    }
+  });
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}/`);
 });
